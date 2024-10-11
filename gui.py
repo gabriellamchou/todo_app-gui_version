a11fbf30dcb67900d0b57a91922c90da9ba05d13
Gabriel <gabriel.lamela.ch@gmail.com>
2024-10-11 11:48:18 +0200

    Implementar botón "Add"
    Leer el contenido de la caja de texto y almacenarlo en todos.txt cuando el usuario pulse el botón "Add"
    Romper el while loop y cerrar el programa cuando el usuario cierre la ventana
    Formatear el texto de la ventana

--- gui.py
+++ gui.py
@@ -2,9 +2,24 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do: ")
-input_box = sg.InputText()
+input_box = sg.InputText(key='todo')
 add_button = sg.Button("Add")
 
-window = sg.Window("My To-Do App", layout=[[label, input_box, add_button]])
-window.read()
+window = sg.Window("My To-Do App",
+                   layout=[[label, input_box, add_button]],
+                   font=("Helvetica", 15))
+
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = value['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
